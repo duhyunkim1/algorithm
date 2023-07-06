cb9c19360a052aff8f40d6927a382e3d279e27ab
duhyunkim1 <duhyunkim@unist.ac.kr>
2023-07-06 20:50:56 +0900

    폰켓몬

---
+++ programmers/Lv.1/해시/폰켓몬.py
@@ -0,0 +1,10 @@
+def solution(participant, completion):
+    dictionary = {}
+    value = 0
+    for p in participant:
+        dictionary[hash(p)] = p
+        value += hash(p)
+        
+    for c in completion:
+        value -= hash(c)
+    return dictionary[value]
