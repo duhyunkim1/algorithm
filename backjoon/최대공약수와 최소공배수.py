39661eb9980440db4e479d1a0fa7acd3e8fc893c
duhyunkim1 <duhyunkim@unist.ac.kr>
2024-02-20 22:49:30 +0900

    최대공약수와 최소공배수

--- backjoon/최대공약수와 최소공배수.py
+++ backjoon/최대공약수와 최소공배수.py
@@ -1,13 +1,9 @@
-import sys
-num1, num2 = map(int, sys.stdin.readline().split(' '))
-
-for n in range(num1, 0, -1):
-    if num1%n == 0 and num2%n == 0:
-        print(n)
-        break
-i = 1
-while True: 
-    if num1*i % num2 == 0:
-        print(num1*i)
-        break
-    i += 1
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a%b)
+num1, num2 = map(int, input().split(' '))
+g = gcd(num1, num2)
+print(g)
+print(num1*num2//g)
