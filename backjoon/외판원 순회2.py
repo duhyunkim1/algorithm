a3d1813faf35c9c4aab368f6922a0ae1f130cdf0
duhyunkim1 <duhyunkim@unist.ac.kr>
2024-02-28 15:02:20 +0900

    외판원 순회2

---
+++ backjoon/외판원 순회2.py
@@ -0,0 +1,35 @@
+def calcuate(a, w):
+    i = 0
+    value = 0
+    a.append(a[0])
+    while i < len(a)-1:
+        if w[a[i]][a[i+1]] == 0:
+            return False
+        value += w[a[i]][a[i+1]]
+        i += 1 
+    return value
+
+def permutation(ls, n, nums, visited, w):
+    if len(ls) == n:
+        tmp = calcuate(ls, w)
+        
+        global answer
+        if tmp is not False and answer > tmp:
+            answer = tmp
+        return
+    for idx, num in enumerate(nums):
+        if not visited[idx]:
+            visited[idx] = True
+            permutation(ls+[num], n, nums, visited, w)
+            visited[idx] = False
+    return
+
+n = int(input())
+w = []
+for _ in range(n):
+    w.append(list(map(int, input().split(' '))))
+a = [x for x in range(n)]
+answer = 1000000*n
+visited = [False for _ in range(n+1)]
+permutation([], n, a, visited, w)
+print(answer)
