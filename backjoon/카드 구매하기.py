081bbe9274615c7c278756d4b222ac68c6aad305
duhyunkim1 <duhyunkim@unist.ac.kr>
2024-03-13 01:57:42 +0900

    카드 구매하기

---
+++ backjoon/카드 구매하기.py
@@ -0,0 +1,10 @@
+dp = [0 for _ in range(1001)]
+n = int(input())
+cards = [0]
+cards+= list(map(int, input().split(' ')))
+
+for i in range(2, n+1):
+    for k in range(i):
+        cards[i] = max([cards[i], cards[i-k]+cards[k]])
+        
+print(cards[-1])
