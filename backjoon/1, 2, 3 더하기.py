ff0fffe2ed6678268d90b3b8d96d9c8ab19c9732
duhyunkim1 <duhyunkim@unist.ac.kr>
2024-03-18 01:56:31 +0900

    1, 2, 3 더하기 5

--- backjoon/1, 2, 3 더하기.py
+++ backjoon/1, 2, 3 더하기.py
@@ -1,18 +1,9 @@
-def plus(x, goal):
-    if x > goal:
-        return
+dp = [0 for _ in range(11)]
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+for i in range(4, 11):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
     
-    if x == goal:
-        global answer
-        answer+=1
-        return 
-    
-    for i in range(1,4):
-        plus(x+i, goal)
-
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    answer = 0
-    plus(0, n)
-    print(answer)
+for _ in range(int(input())):
+    print(dp[int(input())])
